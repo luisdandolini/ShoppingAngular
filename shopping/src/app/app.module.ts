7e90e90dbe0e9e7fdaa276afad6d9e7517036a2d
Luis Dandolini <luisfernandodandoliniduarte@gmail.com>
2022-08-22 17:27:57 -0300

    criando component shoppingList

--- shopping/src/app/app.module.ts
+++ shopping/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ShoppingListComponent } from './components/shoppingList/shoppingList.component';
+import { RecipeBookComponent } from './components/recipeBook/recipeBook.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    RecipeBookComponent
   ],
   imports: [
     BrowserModule,
---
+++ shopping/src/app/components/recipeBook/recipeBook.component.ts
@@ -0,0 +1,11 @@
+import { Component,   } from "@angular/core";
+
+@Component({
+  selector: 'app-recipe-book',
+  templateUrl: './recipeBook.component.html',
+  styleUrls: ['./recipeBook.component.css']
+})
+
+export class RecipeBookComponent {
+
+}
